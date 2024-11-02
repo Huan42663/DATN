5b9903aece4fb769d44ce18175a13424e09c35d4
phanhung <hungpvph42795@fpt.edu.vn>
2024-11-02 09:42:40 +0700

    cap nhat banner va register, login

--- routes/api/client.php
+++ routes/api/client.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Authenticate\AuthController;
+use App\Http\Controllers\Client\AuthController as ClientAuthController;
 use App\Http\Controllers\Client\ProductController;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
@@ -20,8 +22,11 @@ use App\Http\Controllers\Client\PostController;
 */
 
 Route::middleware('auth:sanctum')->get('/user',function(Request $request){
-    return $request->user(); 
+    return $request->user();
 });
+
+Route::post('/login', [ClientAuthController::class, 'login']);
+Route::post('/register', [ClientAuthController::class, 'register']);
 
 Route::apiResource('posts', PostController::class);
 // tạo đơn hàng
