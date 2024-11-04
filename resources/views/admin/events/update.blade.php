5a00acf220d6cf592a1607bd35ec8c1d0397ed60
Hoang Huan <hoanghuan2002ake@gmail.com>
2024-11-04 16:24:40 +0700

    update views events

--- resources/views/admin/events/update.blade.php
+++ resources/views/admin/events/update.blade.php
@@ -1,3 +1,109 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+@extends('admin.master')
+
+@section('title', 'sửa sự kiện')
+
+@section('model', 'Event')
+
+@section('function', 'Update')
+
+@section('content')
+    <div class="main-content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card border-top-0">
+                    <div class="tab-content">
+                        <div class="card-body personal-info">
+                            <div class="mb-4 d-flex align-items-center justify-content-between">
+                                <h5 class="fw-bold mb-0 me-4">
+                                    <span class="d-block mb-2">Update Event:</span>
+                                </h5>
+                            </div>
+                            <form action="">
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-lg-4">
+                                        <label for="fullnameInput" class="fw-semibold">Event Name: </label>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" id="fullnameInput"
+                                                placeholder="Event Name">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-lg-4">
+                                        <label for="fullnameInput" class="fw-semibold">Date Start: </label>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="input-group">
+                                           <input type="datetime-local" class="form-control" name="" id="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-lg-4">
+                                        <label for="fullnameInput" class="fw-semibold">Date End: </label>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="input-group">
+                                            <input type="datetime-local" class="form-control" name="" id="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-lg-4">
+                                        <label for="fullnameInput" class="fw-semibold">Type Event: </label>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="input-group">
+                                            <select name="" class="form-control" id="">
+                                                <option value="0">giảm giá</option>
+                                                <option value="1">giảm giá</option>
+                                                <option value="2">giảm giá</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4 align-items-center">
+                                    <div class="col-lg-4">
+                                        <label for="fullnameInput" class="fw-semibold">ADD Products: </label>
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <table id="example" class="table table-striped" style="width:100%">
+                                            <thead>
+                                                <tr>
+                                                    <th></th>
+                                                    <th>Name</th>
+                                                    <th>Position</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td><input type="checkbox" name="" id=""></td>
+                                                    <td>System Architect</td>
+                                                    <td>Edinburgh</td>
+                                                </tr>
+                                            </tbody>
+                                            <tfoot>
+                                                <tr>
+                                                    <th></th>
+                                                    <th>Name</th>
+                                                    <th>Position</th>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-sm btn-light-brand">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        new DataTable('#example');
+    </script>
+@endsection
